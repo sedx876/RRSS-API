83a3d67d4f36fd10ef947d6fa0485cf35dba047e
sedx876 <swatkins876@gmail.com>
2021-07-05 10:31:13 -0400

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :users, only: :create
+  resources :books
+  resources :comments, only: :index
+  resources :friendships
+  post '/auth/login', to: 'authentication#login'
+  get '/auth/verify', to: 'authentication#verify'
+  put '/comments/:comment_id/books/:id', to: 'books#add_comment'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
